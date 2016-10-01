a1fa67e9cfaa49dbb71b87d712cdbb84a63028b9
Dhruv Patel <dpatel@gasbuddy.com>
2016-10-01 01:15:34 -0600

    Configured dust templating

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ options = {
 app = module.exports = express();
 app.use(kraken(options));
 app.on('start', function () {
-    console.log('Application ready to serve requests.');
-    console.log('Environment: %s', app.kraken.get('env:env'));
+  console.log('Application ready to serve requests.');
+  console.log('Environment: %s', app.kraken.get('env:env'));
 });
